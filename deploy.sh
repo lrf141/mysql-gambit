a22a1bd2d9fb6e264a06269d7c618bc56aeb3b22
lrf141 <ghost141.kentyo@gmail.com>
2020-01-09 12:55:03 +0900

    コンテナ周りの設定を変更

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ fileStatus=`ls ./mysql-8.0.18/plugin_output_directory | grep $sharedLibName`
 
 if [ $fileStatus != "" ]; then
     cp ./mysql-8.0.18/plugin_output_directory/$sharedLibName ./docker/mysql/plugin/$sharedLibName
+    docker exec -it mysql cp /tmp/plugin/ha_gambit.so /usr/lib/mysql/plugin/ha_gambit.so
     docker exec -it mysql mysql -e "INSTALL PLUGIN gambit SONAME '${sharedLibName}'"
     docker exec -it mysql mysql -e "SHOW ENGINES"
 fi
